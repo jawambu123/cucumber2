bd457dba31d914abe12d20d8f36b1d1a1fe54707
Mkamau2008 <jawambu123@gmail.com>
2021-05-08 21:05:49 -0700

    cucumbers

---
+++ mycucumber_project/src/test/java/cucumberOptions/ChangeBackgroundTestRunner.java
@@ -0,0 +1,12 @@
+package cucumberOptions;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "C:\\Users\\njewa\\eclipse-workspace\\TestNg_project\\drivers\\chromedriver.exe")
+public class ChangeBackgroundTestRunner {
+
+}
